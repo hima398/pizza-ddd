629ab97fd958a4e7023b7a22f50d2b54105a6231
hinata398 <r.hinata0398@gmail.com>
2022-08-26 21:48:14 +0900

    initial

---
+++ src/main/java/hima398/pizzaddd/PizzaDddApplication.java
@@ -0,0 +1,13 @@
+package hima398.pizzaddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hima398/pizzaddd/PizzaDddApplicationTests.java
@@ -0,0 +1,13 @@
+package hima398.pizzaddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
